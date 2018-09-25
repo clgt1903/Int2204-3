170efd97f9921648d751541c6bdd5f0a495a5372
CODING GAME C++ <37512124+ThairubyNHT28@users.noreply.github.com>
2018-09-25 22:43:35 +0700

    Add files via upload

---
+++ Group2/17021012/lab03/Boss.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tonghopdoituong;
+
+/**
+ *
+ * @author Nguyễn Thúy
+ */
+public class Boss {
+    String tenBoss;
+    int tuoiBoss;
+    public void TrongNha(){
+        System.out.println("Boss lam nung, trung thanh va coi nha tot");
+    
+    
+}
+}
