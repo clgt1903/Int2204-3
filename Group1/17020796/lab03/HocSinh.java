d27f6a9b81e82bf3ff29c5ac612ae208181c3364
103B_Project <37410268+dangbahuy99@users.noreply.github.com>
2018-09-25 21:12:54 +0700

    Add files via upload

---
+++ Group1/17020796/lab03/HocSinh.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Huy
+ */
+public class HocSinh {
+        private String ten;
+	private int MSSV;
+	private String ngaysinh;
+	private String Khoa;
+
+    /**
+     * @return the ten
+     */
+    public String getTen() {
+        return ten;
+    }
+
+    /**
+     * @param ten the ten to set
+     */
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    /**
+     * @return the MSSV
+     */
+    public int getMSSV() {
+        return MSSV;
+    }
+
+    /**
+     * @param MSSV the MSSV to set
+     */
+    public void setMSSV(int MSSV) {
+        this.MSSV = MSSV;
+    }
+
+    /**
+     * @return the ngaysinh
+     */
+    public String getNgaysinh() {
+        return ngaysinh;
+    }
+
+    /**
+     * @param ngaysinh the ngaysinh to set
+     */
+    public void setNgaysinh(String ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    /**
+     * @return the Khoa
+     */
+    public String getKhoa() {
+        return Khoa;
+    }
+
+    /**
+     * @param Khoa the Khoa to set
+     */
+    public void setKhoa(String Khoa) {
+        this.Khoa = Khoa;
+    }
+}
