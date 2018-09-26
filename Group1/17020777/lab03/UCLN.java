a09f51f7ad6635a0fe020109eea63cfdaa5889af
Manh-Hoang-07 <43363434+Manh-Hoang-07@users.noreply.github.com>
2018-09-26 14:30:26 +0700

    Add files via upload

---
+++ Group1/17020777/lab03/UCLN.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucln;
+
+/**
+ *
+ * @author MANH HOANG
+ */
+
+import java.util.Scanner;
+
+public class UCLN {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int a,b;
+	a=input.nextInt();
+	b=input.nextInt();
+	int c=a;
+	while(b!=0)
+	{
+		c=a%b;
+		a=b;
+		b=c;
+	}
+	System.out.println(a);
+}
+
+}
+    }
+    
+}
